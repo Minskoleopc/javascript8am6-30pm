fff80c88f89fefad29256aeedb29440f6b54c538
unknown <Minskoleopc>
2023-08-08 19:55:09 -0700

    added

---
+++ script.js
@@ -0,0 +1,68 @@
+
+// program 1
+console.log('hello')
+let x = 10
+console.log(x)
+x = 200
+console.log(x)
+
+// program 2 
+
+const j = 100
+console.log(j)
+//j = 900
+
+
+// program 3
+// Arithmetic operation 
+// + , - , * , / , %
+
+let a = 10
+let b = 5
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+let q = 8
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(6,3)
+Calculator(8,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,106 @@
+// // program 1
+// let  x = 10
+// console.log(x)
+// x = 100
+// console.log(x)
+
+// // program 2
+// const j = 10
+// console.log(j)
+// //j = 90
+
+// // Arithmetic operation
+
+// let a = 10
+// let b = 5
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// let s = 8
+// let t = 4
+
+// console.log(s+t)
+// console.log(s-t)
+// console.log(s*t)
+// console.log(s/t)
+// console.log(s%t)
+
+
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(10,5)
+// Calculator(8,4)
+
+
+// program 1
+// function without parameter and without return type 
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+addA()
+
+// function with parameter and without return 
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,4)
+addB(8,4)
+addB(4,4)
+
+
+// function with parameter and with return type 
+// program 3
+// 100 Given 
+// 100 shown
+// Given --- add , sub , divide , calculate , multiple
+
+function addC(x,y){
+    return x + y
+}
+let q1 = addC(12,3)
+console.log(q1)
+console.log(q1 + q1)
+console.log(q1 - q1)
+console.log(q1  / 5)
+console.log(q1 * 0.10)
+
+
+// SQL - 4  8 pm ist 
+// Javascript 6:30 ist 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
