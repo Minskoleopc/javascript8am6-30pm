124f00049363f66fcead994f60bab8dcee7e65ec
Minskoleopc <minskoleopc@gmail.com>
2023-08-11 19:00:33 +0530

    added

---
+++ script4.js
@@ -0,0 +1,79 @@
+// Introvert        // Extrovert
+
+// calm             // speaks more
+// less social      // more social
+// less outing      // more outing 
+
+// Human 
+// Properties - age, weight , height , color , gender
+// Action - walk() , talk() 
+
+// Vehicle - 
+// properties - color , regNo , type ,model , compnayName
+// Method - start() , stop()
+
+// Bank acc -
+// Properties - accNo , accnName , balance , type 
+// Method - withdrawl() , deposit()
+
+
+let x  = 10 
+console.log(x)
+console.log(typeof 10)
+// 0 , 1 , -1 ,1.89,-89.8
+
+let y = true
+console.log(y)
+console.log(typeof y)
+//true or false
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+//" " , ' ' , ``
+// "A" , "a" , '#%%%#' , " ", "asdA123!@#@"
+
+console.log(typeof 10)
+console.log(typeof '10')
+
+// comparison operator
+// entity < entity ------> boolean  True or false
+
+// < , > , <= , >=
+// == , !=           - value 
+// === , !==         - value and type
+
+console.log(5 == '5')
+console.log(5 === '5')
+console.log(5 === 5)
+console.log(5 !== '5')
+console.log(5 !== 5)
+console.log(5 != 5)
+console.log(5 != '5')
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 <= 1)
+console.log(2 >= 1)
+console.log(2 >= 2)
+console.log(2 <= 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
