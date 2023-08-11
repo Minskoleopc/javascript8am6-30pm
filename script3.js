09784cd6ec57729e523609dc7a2946b6d9a6c234
unknown <Minskoleopc>
2023-08-10 19:59:14 -0700

    added

---
+++ script3.js
@@ -0,0 +1,159 @@
+// let x = 10
+// console.log(x)
+// x  = 1000
+// console.log(x)
+
+
+// const g = 100
+// console.log(g)
+// //g = 100
+
+
+// // Arithmetic operator
+
+// let x1 = 10
+// let x2  = 5
+
+// console.log(x1+x2)
+// console.log(x1-x2)
+// console.log(x1*x2)
+// console.log(x1/x2)
+// console.log(x1%x2)
+
+// let s = 8
+// let t = 4
+
+// console.log(s+t)
+// console.log(s-t)
+// console.log(s*t)
+// console.log(s/t)
+// console.log(s%t)
+
+
+// // function 
+
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(8,4)
+
+
+// // function without parameter and without return type 
+
+// function addA(){
+//     console.log(9+9)
+// }
+// addA()
+// addA()
+// addA()
+// addA()
+// addA()
+
+// // function with parameter and without return type 
+// function addB(x,y){
+//     console.log(x+y)
+// }
+// addB(12,4)
+// addB(12,1)
+// addB(12,6)
+
+// // function with paramter and with return type 
+
+// function addC(x,y){
+//     return x + y
+// }
+// q1 = addC(12,6)
+// console.log(q1)
+// console.log(q1 + q1)
+// console.log(q1 + q1 + q1)
+
+
+
+// day 3
+// function declaration
+function subA(x,y){
+    return x - y
+}
+q2 = subA(12,4)
+console.log(q2)
+
+// function expression
+let subB = function(x,y){
+    return x - y
+}
+q3 = subB(12,4)
+console.log(q3)
+
+// arrow function
+let subC = (x,y)=>{
+    return x - y
+}
+q4 = subC(12,4)
+console.log(q4)
+
+
+// expression
+let mul = function(x,y){
+    return x * y
+}
+mul(12,5)
+
+// declartion
+function mulA(x,y){
+    return x * y
+}
+mulA()
+
+// arrow
+let mulB = (x,y)=>{
+    return x * y
+}
+mulB(12,5)
+
+// comparison operator ----> 6:30pm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
