1c0e7df4b4b78aaf22630702af73e84e79d7f14c
unknown <Minskoleopc>
2023-08-11 20:20:19 -0700

    added

---
+++ script4.js
@@ -0,0 +1,94 @@
+// type 
+
+// Introvert          // Extrovert
+
+// calm              // loud
+// less social      // more social
+// less outing      // more outing 
+
+
+
+// Human -
+// Properties - age  , color , gender , height , weight 
+// Method - walk() , talk() 
+
+// Vehicle 
+// Properties - color  ,type , mode , regNo , companyName
+// Method - start()  , stop()
+
+// Bank -
+// poperties - accNo , accName , bal , IFSC code , branchName 
+// Method - withdrawl() , deposit()
+
+
+let x = 10
+console.log(x)
+console.log(typeof x)
+// 0 , 1 , -4,-67.77 , -66.89
+
+let y = true
+console.log(y)
+console.log(typeof y)
+// boolean ---  true or false
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+// " ", '' , ``
+//"A" , "!" , "1" , "a", "qa12" , "QWWERT123@123" 
+
+console.log(typeof 10)
+console.log(typeof '10')
+
+// comparison operator 
+// entity < entity   ======> boolean
+
+
+// < , > , <= , >=
+// == , !=                 - value
+// === , !==               - value and type 
+console.log(6 == '6')
+console.log(6 === '6')
+console.log(6 === 6)
+console.log(6 !== '6')
+console.log(6 == '6')
+console.log(6 != '6')
+console.log(6 != 6)
+console.log(7 > 6)
+console.log(7 < 6)
+console.log(7 != 6)
+console.log(7 <= 6)
+console.log(7 >= 6)
+console.log(7 >= 7)
+console.log(7 <= 7)
+
+// sql 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
