039641abcbb7e25d866557b98f7db9e8badb0140
unknown <Minskoleopc>
2023-08-14 19:47:03 -0700

    added

---
+++ revision.js
@@ -0,0 +1,82 @@
+let x = 10
+console.log(x)
+x = 100
+console.log(x)
+
+const j = 100
+console.log(j)
+//j  = 100
+
+
+// program 2
+
+let a = 10
+let b = 5 
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(23 % 5)
+
+let s = 8
+let t = 4
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(12,4)
+Calculator(6,3)
+Calculator(4,2)
+
+
+// function without parameter and without return type 
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+addA()
+addA()
+
+// function with parameter and with return type 
+
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,4)
+addB(12,1)
+addB(12,3)
+
+// function with parameter and with return type 
+
+function addC(x,y){
+    return x + y
+}
+let q1 = addC(12,6)
+console.log(q1)
+console.log(q1 + q1)
+console.log(q1 + q1 + q1)
+
+
+
+
+
+
+
+
+
+
