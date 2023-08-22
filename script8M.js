8ae960934cca6cf55fe6cbeee6c6eef48b160659
unknown <Minskoleopc>
2023-08-21 19:49:47 -0700

    added

---
+++ script8M.js
@@ -0,0 +1,88 @@
+ // loop
+//  console.log(1)
+//  console.log(2)
+//  console.log(3)
+//  console.log(4)
+//  console.log(5)
+
+//  for(let i  = 1  ; i <=100 ; i++){
+//     console.log(i)
+//  }
+
+
+// for loop 
+
+// for(intialization ; conditionCheck; increment/decrement){
+//     // statement
+// }
+
+for(let i = 1 ; i <= 3 ; i++){ // 2  //3 // 4
+    console.log(i) // 1 // 2 // 3
+}
+
+// 1 <= 3 ------ True 
+// 2 <= 3 ------ True
+// 3 <= 3 ------ True
+
+// program 3
+// print "hello"  3 times 
+for(let i = 1 ; i <= 3 ; i++){ // 2 // 3 // 4
+    console.log("hello") // "hello" , "hello" , "hello"
+}
+
+// program 4
+// print 1 to 5
+
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4 // 5
+}
+
+// program 5
+// print 5 to 1
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// program 6 
+// table of 2 
+// 2,4,6,8,10,12,14,16,18,20
+
+for(let i = 1 ; i <= 10 ; i++){
+    console.log(i * 2)
+}
+
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6  -------22
+        console.log(i) // 2 // 4 ---------------- 20
+}
+
+// program 7
+// table of 5 in reverse
+
+for(let i = 50 ; i >= 5 ; i = i - 5){ // 45 // 40 ------ 0
+    console.log(i) // 50 45 ------------- 5
+}
+
+for(let i = 10 ; i >= 1 ; i --){  // 9
+    console.log(i*5) 
+}
+
+// wednesday -------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// while loop
